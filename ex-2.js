7fe43f372273e451b7d487c03fbe25dd12b77c24
Ice <suthammakit.p@gmail.com>
2025-05-24 16:59:21 +0700

    JavaScript Callbacks and Higher Order Functions Assignment

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,33 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
+function atLeastFive(array, target) {
+  let roomResult = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > target) {
+      roomResult.push("true")
+    }
+  }
+  return roomResult.length
+} 
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+// Using `atLeastFive` function here
+function result (callbackOperation, room, score, roomNumber) {
+    if (callbackOperation(room,score) > 4) { 
+      return 'นักเรียนห้องที่ ' + roomNumber + ' ผ่านเกณฑ์ ✅'
+    } else {
+      return 'นักเรียนห้องที่ ' + roomNumber + ' ไม่ผ่านเกณฑ์ ❌'
+    }
+}
+
+let scoreRoom1Result = result(atLeastFive,studentScoresRoom1,70,1);
+let scoreRoom2Result = result(atLeastFive,studentScoresRoom2,70,2);
+let scoreRoom3Result = result(atLeastFive,studentScoresRoom3,70,3);
+
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
